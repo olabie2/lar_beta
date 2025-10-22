ab83fba46e45651491860b119106934cb2755415
mohammed olabie <olabiedev@gmail.com>
2025-10-22 22:35:29 +0300

    added facebook and linkedin links in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -47,27 +47,21 @@
         </div>
 
         <!-- Bottom Bar: Copyright and Socials -->
-        <div class="mt-12 pt-8 border-t border-glass-border flex flex-col sm:flex-row items-center justify-between">
+       <div class="mt-12 pt-8 border-t border-glass-border flex flex-col sm:flex-row items-center justify-between">
             <p class="text-text-secondary text-sm">
                 &copy; {{ date('Y') }} HOPn. {{ __('footer.copyright') }}
             </p>
             <div class="flex items-center space-x-4 mt-4 sm:mt-0">
              
-                <a href="#" class="text-text-secondary hover:text-primary-light transition-colors">
-                    <span class="sr-only">{{ __('footer.twitter') }}</span>
-                    <!-- Heroicon: X (Twitter) -->
+                <a href="https://www.facebook.com/share/17YbqhqEu3/?mibextid=wwXIfr" class="text-text-secondary hover:text-primary-light transition-colors">
+                    <span class="sr-only">Facebook</span>
+                    <!-- Facebook Icon -->
                     <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231L18.244 2.25zM16.949 19.5h1.57l-6.556-8.72L5.43 4.5H3.86l6.24 8.25L16.949 19.5z"/>
+                        <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
                     </svg>
                 </a>
-                <a href="#" class="text-text-secondary hover:text-primary-light transition-colors">
-                    <span class="sr-only">{{ __('footer.github') }}</span>
-                     <!-- Heroicon: GitHub -->
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.165 6.839 9.49.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.031-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.338 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.001 10.001 0 0022 12c0-5.523-4.477-10-10-10z" clip-rule="evenodd"/>
-                    </svg>
-                </a>
-                <a href="#" class="text-text-secondary hover:text-primary-light transition-colors">
+              
+                <a href="https://www.linkedin.com/company/hopn-ug" class="text-text-secondary hover:text-primary-light transition-colors">
                     <span class="sr-only">{{ __('footer.linkedin') }}</span>
                     <!-- Heroicon: LinkedIn -->
                     <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
